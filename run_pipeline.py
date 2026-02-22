7edef4d2c2d0ddebd42ff7481faa235d4cde219a
yesyese <nagamohan765@gmail.com>
2026-02-22 12:47:50 +0530

    feat: Implement logging, error handling, and execution time tracking within the data pipeline.

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,24 +1,42 @@
-
-
+import logging
+import sys
+from datetime import datetime
 from transform_data import combine_data
 from database_manager import save_to_sqlite, save_to_parquet
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler("etl_pipeline.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+logger = logging.getLogger(__name__)
+
 def run_pipeline():
-    print("Starting data pipeline...")
+    logger.info("Starting data pipeline execution...")
+    
+    try:
+        start_time = datetime.now()
+        
+        logger.info("Extracting and Transforming data (API calls)...")
+        df = combine_data()
+        logger.info(f"Successfully processed data for {len(df)} cities.")
 
-    print("Combining weather and pollution data...")
-    df = combine_data()
-    print("Data combined successfully")
+        logger.info("Loading data into SQLite...")
+        save_to_sqlite(df)
+        
+        logger.info("Loading data into Parquet format...")
+        save_to_parquet(df)
 
-    print("Saving data to SQLite database...")
-    save_to_sqlite(df)
-    print("Data saved to SQLite")
+        end_time = datetime.now()
+        duration = end_time - start_time
+        logger.info(f"Pipeline completed successfully in {duration.total_seconds():.2f} seconds!")
 
-    print("Saving data to Parquet file...")
-    save_to_parquet(df)
-    print("Data saved as Parquet")
-
-    print("Pipeline completed!")
+    except Exception as e:
+        logger.error(f"Pipeline failed! Error: {str(e)}", exc_info=True)
+        sys.exit(1)
 
 if __name__ == "__main__":
     run_pipeline()
